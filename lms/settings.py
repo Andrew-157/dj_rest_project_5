97387a9508b4cbb95f35239f2e75fb3e619ade8e
Andrew Subotin <subotinandrey5@gmail.com>
2023-12-04 16:19:28 +0100

    feat: custom UserCreate and User Serializers inherited from Djoser serializers

--- lms/settings.py
+++ lms/settings.py
@@ -137,3 +137,11 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'users.serializers.UserCreateSerializer',
+        'current_user': 'users.serializers.UserSerializer'
+    }
+}
